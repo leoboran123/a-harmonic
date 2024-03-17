6cadb669713911fa9b5547372776fd3b5f77b885
leoboran123 <erdem_boran_badd@hotmail.com>
2024-03-17 11:24:58 +0300

    static files added...

--- a_harmonic/settings.py
+++ a_harmonic/settings.py
@@ -124,7 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'statics'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # static upload files...
 MEDIA_URL = '/uploads/'
